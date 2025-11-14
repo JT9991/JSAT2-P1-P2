494acaa8181ea3a8d047c287521a4fbb2a392567
Jeff Teoh <jeff.teoh.01@gmail.com>
2025-11-14 16:01:54 +1100

    P2Q2 Removing Summary

--- Part 2/GroupD/P2Q2.js
+++ Part 2/GroupD/P2Q2.js
@@ -7,3 +7,6 @@ console.log(myMovie);
 myMovie.rating = 4;
 myMovie.summary = "World War II movie about the Normandy landings";
 console.log(myMovie);
+
+delete myMovie.summary;
+console.log("After removing summary;", myMovie);
