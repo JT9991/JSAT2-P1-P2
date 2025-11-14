b1bb26f39907db37464d7c8146fe071a5c0db620
Jeff Teoh <jeff.teoh.01@gmail.com>
2025-11-14 15:56:09 +1100

    P2Q2 Update Year and Rating

--- Part 2/GroupD/P2Q2.js
+++ Part 2/GroupD/P2Q2.js
@@ -1,9 +1,9 @@
 let myMovie = {
   title: "The longest day",
-  year: 1964
+  year: 1962
 };
 console.log(myMovie);
 
-myMovie.rating = 5;
+myMovie.rating = 4;
 myMovie.summary = "World War II movie about the Normandy landings";
 console.log(myMovie);
